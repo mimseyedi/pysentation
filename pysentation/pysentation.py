8a4c388da1a225bbe969f2d7d734eff5242021aa
Mohammad <mim.seyedi@gmail.com>
2023-08-13 21:26:56 +0330

    The option_manager function was written and the main function was completed.

--- pysentation/pysentation.py
+++ pysentation/pysentation.py
@@ -12,9 +12,12 @@ pysentation Github repository: https://github.com/mimseyedi/pysentation
 
 import os
 import click
+from pathlib import Path
 from getkey import getkey, keys
+from rich import print as cprint
 from module import Pysentation, PysentationScreen
 from errors import PysentationError
+from _version import __version__
 
 
 def screen_manager(screen: PysentationScreen) -> None:
@@ -68,13 +71,114 @@ def screen_manager(screen: PysentationScreen) -> None:
         pass
 
 
-@click.command(context_settings={'help_option_names': ['-h', '--help']})
+def options_manager(screen: PysentationScreen, options: dict) -> None:
+    """
+    The task of this function is to manage and prioritize options.
+    This function connects options to executive operations according to their priority and can handle a set of options at the same time.
+
+    :param screen: main screen, an object from PysentationScreen.
+    :param options: Acceptable options in dictionary format.
+    :return: None
+    """
+
+    if 'output' in options.keys():
+        if options.get('color'):
+            screen.set_color(color=options.get('color'))
+
+        if options.get('theme'):
+            screen.set_theme(theme=options.get('theme'))
+
+        if options.get('disable'):
+            screen.disable_output()
+
+        screen.write_output(output_path=options.get('output'))
+    else:
+        if 'slides' in options.keys() or 'property' in options.keys():
+            for option, value in options.items():
+                if option == 'slides':
+                    cprint(screen.get_slides())
+                elif option == 'property':
+                    cprint(
+                        screen.get_property(
+                            slide_index=int(value) - 1
+                        )
+                    )
+        else:
+            if options.get('color'):
+                screen.set_color(color=options.get('color'))
+
+            if options.get('theme'):
+                screen.set_theme(theme=options.get('theme'))
+
+            if options.get('disable'):
+                screen.disable_output()
+
+            if options.get('from'):
+                screen.start_from(slide_index=options.get('start') - 1)
+            else:
+                screen.display()
+
+            screen_manager(screen=screen)
+
+
+@click.command(
+    context_settings={'help_option_names': ['-h', '--help']},
+    epilog="pysentation Github repo: https://github.com/mimseyedi/pysentation"
+)
 @click.argument('python_file', required=False, nargs=-1)
-@click.option('-v', '--version', is_flag=True, help='Displays the current version of pysentation installed on the system.')
+@click.option('-f', '--from', nargs=1, type=int, help='Start showing the screen from the selected slide.')
+@click.option('-c', '--color', nargs=1, help='Set color for all slides.')
+@click.option('-t', '--theme', nargs=1, help='Set syntax highlighter theme for all slides.')
+@click.option('-d', '--disable', is_flag=True, help='Disable code interpretation for all slides.')
+@click.option('-p', '--property', nargs=1, type=int, help='Display the properties of the selected slide.')
+@click.option('-s', '--slides', is_flag=True, help='Display the slides on the screen with their position.')
+@click.option('-o', '--output', nargs=1, type=Path, help='Writing all slides in order in a text file.')
+@click.option('-v', '--version', is_flag=True, help='Display the current version of pysentation installed on the system.')
 def main(**options):
     """
+    pysentation is a CLI tool for displaying Python presentations.\n
+    Hot keys to manage screen:\n
+    \b
+    right    Next slide.
+    left     Previous slide.
+    up       Highlight top line.
+    down     Highlight bottom line.
+    r        Reset the current slide.
+    shift+r  Reset the screen.
+    f        Go to first slide.
+    l        Go to last slide.
+    q        Quit.
     """
-    pass
+
+    if options['version'] and not options['python_file']:
+        click.echo(__version__)
+
+    elif options['python_file']:
+        if options['version']:
+            click.echo(
+                "Usage: pysentation [OPTIONS] [PYTHON_FILE]...\nTry 'pysentation --help' for help.\n\nError: -v, --version option cannot be used at this stage.")
+        else:
+            pysentation_obj = Pysentation(
+                source=options['python_file'][0]
+            )
+            try:
+                screen: PysentationScreen = pysentation_obj.build()
+
+                options_manager(
+                    screen=screen,
+                    options={
+                        option: value
+                        for option, value in options.items()
+                        if value is not None and value is not False and option != 'python_file'
+                    }
+                )
+            except PysentationError as error:
+                cprint(
+                    f"[bold red]Error:[/bold red] {error}"
+                )
+    else:
+        click.echo(
+            "Usage: pysentation [OPTIONS] [PYTHON_FILE]...\nTry 'pysentation --help' for help.\n\nError: Missing argument 'PYTHON_FILE...'.")
 
 
 if __name__ == '__main__':
