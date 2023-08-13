e0e8f12e7bd662bc8119cd4f0a4355128ee3d719
Mohammad <mim.seyedi@gmail.com>
2023-08-13 11:50:45 +0330

    The screen_manager function was written.

--- pysentation/pysentation.py
+++ pysentation/pysentation.py
@@ -10,37 +10,58 @@ pysentation Github repository: https://github.com/mimseyedi/pysentation
 """
 
 
+import os
 import click
 from getkey import getkey, keys
 from module import Pysentation, PysentationScreen
+from errors import PysentationError
 
 
 def screen_manager(screen: PysentationScreen) -> None:
     """
+    The task of this function is to execute and manage the screen.
+    This function accepts an argument, which is an object of PysentationScreen and manages the screen according to
+    the defined control keys.
+
+    -These keys are as follows:
+
+        'right'      >>> next slide
+        'left'       >>> previous slide
+        'up'         >>> highlight top line
+        'down'       >>> highlight bottom line
+        'r'          >>> reset the current slide
+        'shift+r'    >>> reset the screen
+        'f'          >>> go to first slide
+        'l'          >>> go to last slide
+        'q'          >>> quit
 
     :param screen: An object from PysentationScreen.
     :return: None
     """
+
     try:
         while True:
             input_key = getkey()
 
             match input_key:
-                case keys.UP:
-                    pass
-                case keys.DOWN:
-                    pass
-                case keys.LEFT:
-                    pass
                 case keys.RIGHT:
-                    pass
+                    screen.next_slide()
+                case keys.LEFT:
+                    screen.previous_slide()
+                case keys.UP:
+                    screen.highlight_top_line()
+                case keys.DOWN:
+                    screen.highlight_bottom_line()
                 case 'r':
-                    pass
+                    screen.reset_slide()
+                case 'R':
+                    screen.reset_screen()
                 case 'f':
-                    pass
+                    screen.first_slide()
                 case 'l':
-                    pass
+                    screen.last_slide()
                 case 'q':
+                    os.system('clear' if os.name == 'posix' else 'cls')
                     break
 
     except KeyboardInterrupt:
